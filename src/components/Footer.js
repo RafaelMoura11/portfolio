938238d5da24ed28df5ec8a17e1bf927c9f5c921
Rafael <rafaelmoura7033@gmail.com>
2022-02-04 16:23:21 -0300

    Criando o footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import linkedinIcon from '../images/linkedin.svg';
+import githubIcon from '../images/github.svg'
 
 export default function Footer() {
   return (
-    <footer>
-      Opa
+    <footer className="container-fluid" id="footer">
+      <a href="https://www.linkedin.com/in/rafael-rmoura/" >
+        <img src={ linkedinIcon } alt="linkedInIcon" id="contact" />
+      </a>
+      <p id="email">rafaelmoura7033@gmail</p>
+      <a href="https://github.com/RafaelMoura11" >
+        <img src={ githubIcon } alt="githubIcon" id="contact" />
+      </a>
     </footer>
   )
 }
