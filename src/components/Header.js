7d6f34229d114e77dc55ddc21ef2fb39496f648d
Rafael <rafaelmoura7033@gmail.com>
2022-02-07 09:18:03 -0300

    Alterando estrutura da pagina about e consertando problemas de css

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
   const changePage = ({ target: { name } }) => page(`/${name}`)
 
   return (
-    <nav className="btn-group" id="header" role="group" aria-label="Basic example">
+    <nav id="header">
       <button type="button" className="btn btn-secondary nav-button" name="" onClick={ changePage }>Home</button>
       <button type="button" className="btn btn-secondary nav-button" name="about" onClick={ changePage }>About</button>
       <button type="button" className="btn btn-secondary nav-button" name="skills" onClick={ changePage }>Skills</button>
--- src/my_data/index.js
+++ src/my_data/index.js
@@ -4,7 +4,45 @@ const MY_DATA = {
       intro: 'Olá! Eu sou o Rafael Moura, tenho 20 anos e sou de Recife-PE. Sou um desenvolvedor front end e futuramente fullstack',
       goals: 'Entrei no mundo da programação na metade de 2021 e foi paixão a primeira vista. Meu maior desejo atualmente é conseguir meu primeiro emprego na área e aprender o máximo que puder, seja em Hard Skill ou Soft Skill.',
       formation: 'Irei finalizar meus estudos na Trybe em agosto de 2022. A Trybe é uma escola de programação com mais de 1500 horas de aulas e aborda introdução ao desenvolvimento de software, front-end, back-end, ciência da computação, engenharia de software, metodologias ágeis e habilidades comportamentais.'
-    }
+    },
+
+    skills: [
+      {
+        tech: 'JavaScript',
+        description: 'Linguagem de programação muito utilizada no desenvolvimento web',
+        image: '',
+      },
+      {
+        tech: 'HTML',
+        description: 'Linguagem de marcação de texto',
+        image: '',
+      },
+      {
+        tech: 'CSS',
+        description: 'Linguagem de estilização',
+        image: '',
+      },
+      {
+        tech: 'React',
+        description: 'Biblioteca para criação de interface de usuário',
+        image: '',
+      },
+      {
+        tech: 'Redux',
+        description: 'Ferramenta de controle de estado de aplicações',
+        image: '',
+      },
+      {
+        tech: 'Linux',
+        description: 'Sistema Operacional',
+        image: '',
+      },
+      {
+        tech: '',
+        description: '',
+        image: '',
+      }
+    ]
   }
 };
 
