962bb87cce6f3c78336117f9b72956b6acba6eb8
Rafael <rafaelmoura7033@gmail.com>
2022-07-24 11:57:30 -0300

    Atualizando texto sobre mim

--- src/my_data/index.js
+++ src/my_data/index.js
@@ -3,9 +3,9 @@ import images from "../images";
 const MY_DATA = {
   portuguese: {
     about: {
-      intro: 'Olá! Eu sou o Rafael Moura, tenho 20 anos e sou de Recife-PE. Sou um desenvolvedor front end e futuramente fullstack',
-      goals: 'Entrei no mundo da programação na metade de 2021 e foi paixão a primeira vista. Meu maior desejo atualmente é conseguir meu primeiro emprego na área e aprender o máximo que puder, seja em Hard Skill ou Soft Skill.',
-      formation: 'Irei finalizar meus estudos na Trybe em agosto de 2022. A Trybe é uma escola de programação com mais de 1500 horas de aulas e aborda introdução ao desenvolvimento de software, front-end, back-end, ciência da computação, engenharia de software, metodologias ágeis e habilidades comportamentais.'
+      intro: 'Olá! Eu sou o Rafael Moura e moro em Recife-PE.',
+      goals: 'Entrei no mundo da programação na metade de 2021 e foi paixão a primeira vista. Quando percebi que eu gostava de resolver problemas através da lógica, eu automaticamente entrei de cabeça no mundo da Tecnologia.',
+      formation: 'Irei finalizar meus estudos na Trybe em agosto de 2022. A Trybe é uma escola de programação com mais de 1500 horas de aulas e aborda introdução ao desenvolvimento de software, front-end, back-end, ciência da computação, engenharia de software, metodologias ágeis e habilidades comportamentais.',
     },
 
     skills: [
